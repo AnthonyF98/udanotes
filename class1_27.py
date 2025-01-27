ef42a6eaff6e3b260f2db0501d84f7ae2ce5a3bc
AnthonyF98 <aferna29@nd.edu>
2025-01-27 13:51:05 -0500

    int

---
+++ class1_27.py
@@ -0,0 +1,10 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+import pyarrow
+
+
+lyrics_pd = pd.read_feather(
+  'C:/Users/antfe/OneDrive/Unstructured Analytics/complete_lyrics_2025.feather'
+)
+
